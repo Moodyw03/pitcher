bc7e841f7c877361b553f92c4d7e34aa2675de50
Muhammad Umar <82892724+umarsadiqi@users.noreply.github.com>
2024-04-25 13:36:16 +0500

    Refactor handleOnSubmit function in DownloadAudio component

--- components/download-audio.tsx
+++ components/download-audio.tsx
@@ -16,20 +16,63 @@ export default function DownloadAudio({ audioFile, rate }: Props) {
 
   const speedFactor = rate[0];
 
-  async function create() {
+  async function handleOnSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    if (!audioFile) {
+      alert("Please upload a file.");
+      return;
+    }
+    if (speedFactor <= 0.5) {
+      alert("Please enter a valid speed factor (greater than 0.5).");
+      return;
+    }
+
     const formData = new FormData();
+    formData.append("audioFile", audioFile);
     formData.append("speedFactor", speedFactor.toString());
-    formData.append("audio", audioFile as Blob);
 
-    setIsLoading(true);
+    try {
+      const response = await fetch(
+        "https://audio-speed-changer.onrender.com/api/upload",
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
 
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 2000);
+      if (response.ok) {
+        // Assuming the server sends the processed file as a response
+        response.blob().then((blob) => {
+          const url = window.URL.createObjectURL(blob);
+
+          console.log("Download URL:", url);
+
+          const a = document.createElement("a");
+          a.style.display = "none";
+          a.href = url;
+          // the filename you want
+          a.download =
+            audioFile.name.replace(/\.[^/.]+$/, "") + "-processed.mp3";
+          document.body.appendChild(a);
+          a.click();
+          window.URL.revokeObjectURL(url);
+          alert("Your file has been downloaded!");
+        });
+      } else {
+        response.text().then((text) => {
+          console.error("Server response:", text);
+          alert("Error: " + text);
+        });
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      alert("Error uploading file.");
+    }
   }
 
   return (
-    <form action={create}>
+    <form onSubmit={handleOnSubmit}>
       <div className="relative">
         <div className="absolute transition-all duration-1000 opacity-75 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt"></div>
         <Button
